7f3d6b87634e42eb21b1bba5caddcf2b4bb9844d
MauricioCifuentes2000 <maucifuentes00@live.com.mx>
2025-03-02 18:03:42 -0600

    Añadiendo

---
+++ ej2/JS/efectos.js
@@ -0,0 +1,73 @@
+//1
+/*
+$(document).ready(function(){
+    function efecto() {
+        $(".caja1").hide(3000);
+    }
+    efecto();
+});
+*/
+
+//2
+/*
+$(document).ready(function(){
+    function efecto() {
+        $(".caja1").fadeOut(3000);
+    }
+    efecto();
+});
+*/
+
+//3
+/*
+$(document).ready(function(){
+    function efecto() {
+        $(".caja1").hide();
+        $(".caja1").fadeIn(4000);
+    }
+    efecto();
+});
+*/
+
+//4
+/*
+$(document).ready(function(){
+    function efecto() {
+        $(".caja1").hide();
+        $(".caja1").toggle(4000);
+    }
+    efecto();
+});
+*/
+
+//5
+/*
+$(document).ready(function(){
+    function efecto() {
+        $(".caja1").show();
+        $(".caja1").toggle(4000);
+    }
+    efecto();
+});
+*/
+
+//6
+/*
+$(document).ready(function(){
+    function efecto() {
+        $(".caja1").slideUp(2000);
+    }
+    efecto();
+});
+*/
+
+//7
+/*
+$(document).ready(function(){
+    function efecto() {
+        $(".caja1").hide();
+        $(".caja1").slideDown(2000);
+    }
+    efecto();
+});
+*/
